f832016c9f0b211f7898a2a4d66cf006d2f62af2
kumarAjay10 <144784190+kumarAjay10@users.noreply.github.com>
2023-11-30 22:23:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function functionName(){
+	alert("functionName");
+}
+functionName()
+//your JS code here. If required.
+function AccioJob(){
+	alert("AccioJob");
+}
+AccioJob()
